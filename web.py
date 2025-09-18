c24d0f808de60eb21acc559aa9862555970c9efb
Akil-Pravin <pravinakil86@gmail.com>
2025-09-18 11:44:00 +0530

    Success

--- web.py
+++ web.py
@@ -1,3 +1,6 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+content ='''
 <html>
     <head>
         <title> My Web Server</title>
@@ -22,4 +25,19 @@
         </tr>
         </table>
     </body>
-</html>
+</html>'''
+
+
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("Get request received...")
+        self.send_response(200) 
+        self.send_header("content-type", "text/html")       
+        self.end_headers()
+        self.wfile.write(content.encode())
+
+print("This is my webserver") 
+server_address =('',8000)
+httpd = HTTPServer(server_address,MyServer)
+httpd.serve_forever()
